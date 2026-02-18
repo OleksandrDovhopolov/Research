7344f8637e844f260d73e51212ec5ca2beee5f9f
aDovhopolov <oleksandr.dovhopolov@xflowgames.com>
2026-02-18 20:15:25 +0200

    update IsInitialized value

--- Assets/Scripts/CardCollectionImpl/Demo/CardCollectionEntryPoint.cs
+++ Assets/Scripts/CardCollectionImpl/Demo/CardCollectionEntryPoint.cs
@@ -9,15 +9,18 @@ namespace core
     {
         private CardCollectionModule _cardCollectionModule;
 
-        private bool _isInitialized;
+        private readonly UniTaskCompletionSource _initializationSource = new();
+
+        public bool IsInitialized => _initializationSource.Task.Status.IsCompleted();
+        public UniTask WaitForInitializationAsync() => _initializationSource.Task;
 
         public ICardCollectionModule CardCollectionModule =>
-            !_isInitialized ? throw new InvalidOperationException("Module not initialized.") : _cardCollectionModule;
+            !IsInitialized ? throw new InvalidOperationException("Module not initialized.") : _cardCollectionModule;
         public ICardCollectionUpdater CardCollectionUpdater =>
-            !_isInitialized ? throw new InvalidOperationException("Module not initialized.") : _cardCollectionModule;
+            !IsInitialized ? throw new InvalidOperationException("Module not initialized.") : _cardCollectionModule;
         public ICardCollectionReader CardCollectionReader =>
-            !_isInitialized ? throw new InvalidOperationException("Module not initialized.") : _cardCollectionModule;
-
+            !IsInitialized ? throw new InvalidOperationException("Module not initialized.") : _cardCollectionModule;
+        
         private void Awake()
         {
             IniCardCollection().Forget();
@@ -25,18 +28,25 @@ namespace core
 
         private async UniTask IniCardCollection()
         {
-            ICardPackProvider packProvider = new JsonCardPackProvider();
-            IEventCardsStorage cardsStorage = new JsonEventCardsStorage();
-            ICardDefinitionProvider cardDefinitionProvider = new DefaultCardDefinitionProvider();
-            ICardSelector cardSelector = new ProbabilityBasedCardSelector();
+            try
+            {
+                ICardPackProvider packProvider = new JsonCardPackProvider();
+                IEventCardsStorage cardsStorage = new JsonEventCardsStorage();
+                ICardDefinitionProvider cardDefinitionProvider = new DefaultCardDefinitionProvider();
+                ICardSelector cardSelector = new ProbabilityBasedCardSelector();
 
-            const string testEventId = "test";
-            var config = new CardCollectionModuleConfig(packProvider, cardsStorage, cardDefinitionProvider, cardSelector, testEventId);
+                const string testEventId = "test";
+                var config = new CardCollectionModuleConfig(packProvider, cardsStorage, cardDefinitionProvider, cardSelector, testEventId);
 
-            _cardCollectionModule = new CardCollectionModule(config);
-            await _cardCollectionModule.InitializeAsync();
-            
-            _isInitialized = true;
+                _cardCollectionModule = new CardCollectionModule(config);
+                await _cardCollectionModule.InitializeAsync();
+
+                _initializationSource.TrySetResult();
+            }
+            catch (Exception ex)
+            {
+                _initializationSource.TrySetException(ex);
+            }
         }
     }
 }
