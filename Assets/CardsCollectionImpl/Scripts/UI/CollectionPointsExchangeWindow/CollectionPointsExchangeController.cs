54d063e2de538b070cef8f3031865b606435fdb0
aDovhopolov <dn070291dag@gmail.com>
2026-03-11 12:31:21 +0200

    add inventory prefab

--- Assets/CardsCollectionImpl/Scripts/UI/CollectionPointsExchangeWindow/CollectionPointsExchangeController.cs
+++ Assets/CardsCollectionImpl/Scripts/UI/CollectionPointsExchangeWindow/CollectionPointsExchangeController.cs
@@ -83,6 +83,7 @@ namespace CardCollectionImpl
                 {
                     if (await Args.ExchangeOfferProvider.ReceiveOfferContent(offerPackId, ct))
                     {
+                        //TODO when ContentWidgetController is open add logic gets here = visual bug on close ContentWidgetController. its blink
                         ShowInfoWidget("Pack received successfully");
                         Args.OnPointsAmountChangedHandler?.Invoke();
                         CloseWindow();
