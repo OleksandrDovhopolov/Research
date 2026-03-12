a3a2f2e07fb59b92e8af3b142a7a64a61971d741
aDovhopolov <dn070291dag@gmail.com>
2026-03-12 10:39:00 +0200

    add cleanup method in pull and update view logic

--- Assets/Inventory/Runtime/Implementation/UI/WIndow/InventoryView.cs
+++ Assets/Inventory/Runtime/Implementation/UI/WIndow/InventoryView.cs
@@ -1,11 +1,12 @@
 using Inventory.Implementation.UI;
 using TMPro;
+using UIShared;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Inventory.Implementation
 {
-    public class InventoryView : MonoBehaviour
+    public class InventoryView : MonoBehaviour, ICleanup
     {
         [SerializeField] private Image _image;
         [SerializeField] private Text _titleText;
@@ -40,6 +41,35 @@ namespace Inventory.Implementation
         public void SetSprite(Sprite sprite)
         {
             _image.sprite = sprite;
+        }
+
+        public void Cleanup()
+        {
+            if (_image != null)
+            {
+                _image.sprite = null;
+            }
+
+            if (_titleText != null)
+            {
+                _titleText.text = string.Empty;
+            }
+
+            if (_subtitleText != null)
+            {
+                _subtitleText.text = string.Empty;
+            }
+
+            if (_stackCountText != null)
+            {
+                _stackCountText.text = string.Empty;
+            }
+
+            if (_stackCountTextPro != null)
+            {
+                _stackCountTextPro.gameObject.SetActive(false);
+                _stackCountTextPro.text = string.Empty;
+            }
         }
     }
 }
