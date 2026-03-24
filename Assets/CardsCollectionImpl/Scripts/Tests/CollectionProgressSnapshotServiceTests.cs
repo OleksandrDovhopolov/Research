550a5c86464ac8b4f3319ea8433ff186abc3c2f8
aDovhopolov <dn070291dag@gmail.com>
2026-03-24 11:22:09 +0200

    fix tests

--- Assets/CardsCollectionImpl/Scripts/Tests/CollectionProgressSnapshotServiceTests.cs
+++ Assets/CardsCollectionImpl/Scripts/Tests/CollectionProgressSnapshotServiceTests.cs
@@ -1,21 +1,15 @@
 using CardCollection.Core;
+using System.Collections.Generic;
 using NUnit.Framework;
-using Infrastructure;
 
 namespace CardCollectionImpl
 {
     public class CollectionProgressSnapshotServiceTests
     {
-        [SetUp]
-        public void SetUp()
-        {
-        }
-
         [Test]
         public void TryGetSnapshot_WithoutSet_ReturnsFalse()
         {
-            //TODO fix this 
-            var service = new CollectionProgressSnapshotService(null,null);
+            var service = CreateService(new Dictionary<string, (int collected, int total)>());
 
             var hasSnapshot = service.TryGetSnapshot(out _);
 
@@ -25,8 +19,7 @@ namespace CardCollectionImpl
         [Test]
         public void SetSnapshot_SetsTotalAndCollectedAmounts()
         {
-            //TODO fix this 
-            var service = new CollectionProgressSnapshotService(null, null);
+            var service = CreateService(new Dictionary<string, (int collected, int total)>());
             var data = new EventCardsSaveData();
             data.Cards.Add(new CardProgressData { CardId = "a1", IsUnlocked = true });
             data.Cards.Add(new CardProgressData { CardId = "a2", IsUnlocked = false });
@@ -42,8 +35,10 @@ namespace CardCollectionImpl
         [Test]
         public void SetSnapshot_BuildsPerGroupProgress()
         {
-            //TODO fix this 
-            var service = new CollectionProgressSnapshotService(null, null);
+            var service = CreateService(new Dictionary<string, (int collected, int total)>
+            {
+                ["g1"] = (1, 2)
+            });
             var data = new EventCardsSaveData();
             data.Cards.Add(new CardProgressData { CardId = "card-g1-1", IsUnlocked = true });
             data.Cards.Add(new CardProgressData { CardId = "card-g1-2", IsUnlocked = false });
@@ -56,6 +51,54 @@ namespace CardCollectionImpl
             Assert.AreEqual("Group One", snapshot.GroupProgress[0].GroupName);
             Assert.AreEqual(1, snapshot.GroupProgress[0].CollectedAmount);
             Assert.AreEqual(2, snapshot.GroupProgress[0].TotalAmount);
+        }
+
+        private static CollectionProgressSnapshotService CreateService(
+            Dictionary<string, (int collected, int total)> byGroupType)
+        {
+            var cache = new FakeCardCollectionCacheService(byGroupType);
+            var groups = new List<CardCollectionGroupConfig>
+            {
+                new()
+                {
+                    groupType = "g1",
+                    groupName = "Group One"
+                }
+            };
+
+            return new CollectionProgressSnapshotService(cache, groups);
+        }
+
+        private sealed class FakeCardCollectionCacheService : ICardCollectionCacheService
+        {
+            private readonly Dictionary<string, (int collected, int total)> _byGroupType;
+
+            public FakeCardCollectionCacheService(Dictionary<string, (int collected, int total)> byGroupType)
+            {
+                _byGroupType = byGroupType ?? new Dictionary<string, (int collected, int total)>();
+            }
+
+            public void Initialize(IReadOnlyList<CardConfig> configs) { }
+
+            public IEnumerable<CardProgressData> GetCardsByGroupType(EventCardsSaveData saveData, string groupType)
+            {
+                return new List<CardProgressData>();
+            }
+
+            public int GetGroupAmount(EventCardsSaveData saveData, string groupType)
+            {
+                return _byGroupType.TryGetValue(groupType, out var value) ? value.total : 0;
+            }
+
+            public int GetCollectedGroupAmount(EventCardsSaveData saveData, string groupType)
+            {
+                return _byGroupType.TryGetValue(groupType, out var value) ? value.collected : 0;
+            }
+
+            public List<NewCardDisplayData> ToNewCardDisplayData(List<CardProgressData> cardsData)
+            {
+                return new List<NewCardDisplayData>();
+            }
         }
     }
 }
