3a1bcffc1aa8191d7b54af44230886a516a47fc0
aDovhopolov <dn070291dag@gmail.com>
2026-04-30 22:49:16 +0300

    update project settings

--- Assets/Game/Core/Bootstrap/RemoteConfigLoader.cs
+++ Assets/Game/Core/Bootstrap/RemoteConfigLoader.cs
@@ -4,6 +4,7 @@ using Cysharp.Threading.Tasks;
 using Firebase;
 using Firebase.RemoteConfig;
 using UnityEngine;
+using Unity.Services.Core;
 
 namespace Game.Bootstrap
 {
@@ -11,10 +12,12 @@ namespace Game.Bootstrap
     {
         public bool IsReady { get; private set; }
         private bool _dependenciesReady;
+        private bool _unityServicesReady;
 
         public async UniTask EnsureDependenciesAsync(CancellationToken ct)
         {
             ct.ThrowIfCancellationRequested();
+            await EnsureUnityServicesAsync(ct);
 
             var dependencyStatus = await FirebaseApp.CheckAndFixDependenciesAsync().AsUniTask();
             if (dependencyStatus != DependencyStatus.Available)
@@ -23,6 +26,35 @@ namespace Game.Bootstrap
             }
 
             _dependenciesReady = true;
+        }
+
+        private async UniTask EnsureUnityServicesAsync(CancellationToken ct)
+        {
+            ct.ThrowIfCancellationRequested();
+
+            if (_unityServicesReady)
+            {
+                return;
+            }
+
+            switch (UnityServices.State)
+            {
+                case ServicesInitializationState.Initialized:
+                    _unityServicesReady = true;
+                    return;
+
+                case ServicesInitializationState.Initializing:
+                    await UniTask.WaitUntil(
+                        () => UnityServices.State == ServicesInitializationState.Initialized,
+                        cancellationToken: ct);
+                    _unityServicesReady = true;
+                    return;
+            }
+
+            await UnityServices.InitializeAsync().AsUniTask();
+            ct.ThrowIfCancellationRequested();
+            _unityServicesReady = true;
+            Debug.Log("[RemoteConfigLoader] Unity Gaming Services initialized.");
         }
 
         public async UniTask FetchAndActivateAsync(CancellationToken ct)
