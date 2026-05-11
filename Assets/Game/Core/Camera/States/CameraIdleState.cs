36fb16286a0a3f1041ef5558a5995b485af5e04d
aDovhopolov <dn070291dag@gmail.com>
2026-05-11 15:31:50 +0300

    update hud info

--- Assets/Game/Core/Camera/States/CameraIdleState.cs
+++ Assets/Game/Core/Camera/States/CameraIdleState.cs
@@ -69,7 +69,7 @@ namespace CameraModule
                     ChangeState<CameraOneHandZoomState>();
                     return;
                 }
-                //ChangeState<CameraDragState>();
+                ChangeState<CameraDragState>();
             }
         }
     }
