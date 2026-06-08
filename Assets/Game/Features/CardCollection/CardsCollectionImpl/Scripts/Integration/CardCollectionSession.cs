56b81994320df43058de172935f8691335cd7a0d
aDovhopolov <dn070291dag@gmail.com>
2026-06-08 11:03:04 +0300

    add log TODO

--- Assets/Game/Features/CardCollection/CardsCollectionImpl/Scripts/Integration/CardCollectionSession.cs
+++ Assets/Game/Features/CardCollection/CardsCollectionImpl/Scripts/Integration/CardCollectionSession.cs
@@ -160,6 +160,7 @@ namespace CardCollectionImpl
                 throw new ArgumentException("Schedule item is null or invalid.", nameof(scheduleItem));
 
             // DEBUG: force Spring_Collection label until other collection bundles ship
+            //TODO LIVEOPS bug. if there is no group in addressables - error.
             const string debugLabel = "Spring_Collection";
             await ProdAddressablesWrapper.DownloadDependenciesByLabelAsync(debugLabel, ct);
             ct.ThrowIfCancellationRequested();
